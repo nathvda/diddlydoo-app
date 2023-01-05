2a3d794151b0edf81a501583db137f4956f1831d
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-01-05 13:44:09 +0100

    edit event

---
+++ assets/editEvent.js
@@ -0,0 +1,15 @@
+export default async function editEvent() {
+  try {
+    await fetch("/api/events/[id]/add_dates", {
+      method: "PATCH",
+      headers: { "Content-Type": "application-json" },
+      body: {
+        name: "Henri",
+        dates: ["2022-08-16"],
+        available: true,
+      },
+    }).then(console.log("modification complete"));
+  } catch (e) {
+    console.log(e);
+  }
+}
---
+++ assets/index.js
@@ -0,0 +1,4 @@
+import editEvent from "./editEvent.js";
+
+let test = document.getElementById("test");
+test.addEventListener("click", editEvent);
