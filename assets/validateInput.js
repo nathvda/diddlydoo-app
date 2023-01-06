ca552e40d9863d7b7996dc0805dae04edc4ac9cf
Virginie Dourson <virginiedourson@gmail.com>
2023-01-06 10:23:23 +0100

    Transmission form premiers resultats

--- assets/validateInput.js
+++ assets/validateInput.js
@@ -1,5 +1,16 @@
 //Cette fonction prend un string comme argument
-//et vérifie si ce string contient min 1 caractère, et max 256 caractères.
-export function validateInput(input){
 
+import { log } from "console";
+
+//et vérifie si ce string contient min 1 caractère, et max 256 caractères.
+export function validateInput(){
+    return true;
+    // if (document.myForm.name.value != ""){
+    //     console.log("Champ complété")
+    //     return true;
+    // }
+    // else {
+    //     console.log("Champ vide");
+    //     return false;
+    // }
 }
