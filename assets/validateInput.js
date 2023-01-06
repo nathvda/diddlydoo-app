46ad48d4709ccd38ee634b8219a8c49055541eb0
Virginie Dourson <virginiedourson@gmail.com>
2023-01-06 16:05:51 +0100

    Résolution reset form

--- assets/validateInput.js
+++ assets/validateInput.js
@@ -1,6 +1,7 @@
 //Cette fonction prend un string comme argument
 //et vérifie si ce string contient min 1 caractère, et max 256 caractères.
 export default function validateInput(e){
+    e.preventDefault();
     let form = document.getElementById('myform');
     let err = document.getElementById('errorName');
     let names = ["name", "author", "description"];
@@ -9,16 +10,15 @@ export default function validateInput(e){
         if (form[n].value == "") {
             form[n].nextElementSibling.nextElementSibling.textContent = "This field must be filled";
             form[n].nextElementSibling.nextElementSibling.style.color = 'red';
-            e.preventDefault();
             submit = false;
         }
         else if (form[n].value.length > 256){
             form[n].nextElementSibling.nextElementSibling.textContent = "This field must contain at leat 256 characters";
-            e.preventDefault();
             submit = false;
         } 
     }
     if (submit==true) {
+        form.submit();
         form.reset();
         for (let n of names) {
             form[n].nextElementSibling.nextElementSibling.textContent = ""; 
