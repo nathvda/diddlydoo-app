b1b4d8087de0ed98c4cd479ee2be3b23fe4e95e2
Virginie Dourson <virginiedourson@gmail.com>
2023-01-06 12:24:31 +0100

    Premiere fonction erreur pour input

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,9 @@
 import editElement from "./editElement.js";
 import readElement from "./readElement.js";
 import generateElement from "./displayElement.js";
+import validateInput from "./validateInput.js";
 
 window.addEventListener("load", readElement);
+
+let myForm = document.getElementById('myform');
+myForm.addEventListener('submit', validateInput);
--- assets/validateInput.js
+++ assets/validateInput.js
@@ -1,16 +1,18 @@
 //Cette fonction prend un string comme argument
-
-import { log } from "console";
-
 //et vérifie si ce string contient min 1 caractère, et max 256 caractères.
-export function validateInput(){
-    return true;
-    // if (document.myForm.name.value != ""){
-    //     console.log("Champ complété")
-    //     return true;
-    // }
-    // else {
-    //     console.log("Champ vide");
-    //     return false;
-    // }
+export default function validateInput(e){
+    let form = document.getElementById('myform');
+    let err = document.getElementById('errorName');
+    if (form.name.value == "") {
+        err.textContent = "Le champ doit être complété";
+        err.style.color = 'red';
+        e.preventDefault();
+    }
+    else if (form.name.value.length > 256){
+        err.textContent = "Le champ doit comporter moins de 256 caractères";
+        e.preventDefault();
+    } 
+    else {
+        form.reset();
+    }
 }
