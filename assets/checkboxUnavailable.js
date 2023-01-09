cde8808bf4e04f39509a4fd77372624eb93f35df
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2023-01-09 15:07:35 +0100

    checkbox stuff

--- assets/checkboxUnavailable.js
+++ assets/checkboxUnavailable.js
@@ -1,51 +1,50 @@
-
-
-export default async function checkboxUnavailable (id, date, name, available) {
-   try {
-
+export default async function checkboxUnavailable(id, date, name, available) {
+  try {
     let woop;
 
     await fetch(`http://localhost:3000/api/events/${id}`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json'}
-    }).then((res => res.json()))
-    .then((data => woop = data));
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => (woop = data));
 
     console.log(woop.dates);
 
-    for (let elem of woop.dates){
-      if (elem.date === date){
-        console.log("bonne date");
-        for (let att of elem.attendees){
-          if (att.name === name){
+    console.log(woop);
+
+    let infos = [];
+
+    for (let elem of woop.dates) {
+      for (let att of elem.attendees) {
+        if (att.name === name) {
+          if (elem.date === date) {
             att.available = available;
+            let obj = { date: elem.date, available: att.available };
+            infos.push(obj);
+          } else {
+
+          let obj = { date: elem.date, available: att.available };
+          console.log(obj);
+          infos.push(obj);
           }
+          
+          console.log(infos);
         }
-      } else {
-        console.log(elem.date);
       }
     }
 
-
-    console.log(woop);
-    
-    
-
-
-
-      // await fetch(`http://localhost:3000/api/events/${id}/attend`, {
-      //   method: 'PATCH',
-      //   headers: { 'Content-Type': 'application/json' },
-      //   body: JSON.stringify({
-      //     name: name,
-      //     dates: [{date: date,
-      //       available : available}]
-      //   })
-      // })
-      //   .then((response) => response.json())
-      //   .then(console.log('attendee modified'))
-    } catch (e) {
-      console.log(e)
-    }
+    await fetch(`http://localhost:3000/api/events/${id}/attend`, {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name: name,
+            dates: infos
+          })
+        })
+          .then((response) => response.json())
+          .then(console.log('attendee modified'))
+  } catch (e) {
+    console.log(e);
   }
-  
+}
