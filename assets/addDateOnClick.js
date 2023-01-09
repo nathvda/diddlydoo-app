9ab1eb3f4dc11cf9f949364be1506d86fe95f431
Virginie Dourson <virginiedourson@gmail.com>
2023-01-09 09:13:22 +0100

    ajout de dates lors de la création

---
+++ assets/addDateOnClick.js
@@ -0,0 +1,9 @@
+export default function addDateOnClick(e){
+    e.preventDefault();
+    let form = document.getElementById('myform');
+    let newDate = document.createElement('input');
+    newDate.setAttribute('type', 'date');
+    newDate.setAttribute('name', 'dates[]');
+    newDate.setAttribute('id', 'date2');
+    form.insertBefore(newDate, e.target);
+}
